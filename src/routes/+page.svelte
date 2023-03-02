eadb1bb7e79d570cdbf0022889fdf2af91446ee6
anavollu <anavollu@id.uff.br>
2023-03-02 18:34:08 -0300

    wip: home page > style texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
 	import Button from './button.svelte';
 </script>
 
-<div class="flex flex-col items-center">
-	<p class="uppercase">Desenvolvedora Front-end</p>
-	<p>Ana Carolina Vollu</p>
+<div class="mx-11 my-9 flex h-full flex-col items-center justify-between">
+	<p class="text-center font-inter text-base uppercase tracking-widest text-blue">
+		Desenvolvedora Front-end
+	</p>
+	<p class="text-center font-poppins text-3xl text-darkblue">Ana Carolina Vollu</p>
 	<img src={desktopImage} alt="Imagem de um computador" />
 	<div class="flex flex-col gap-8">
 		<Button label="Projetos" size="large" page="/projetos" />
