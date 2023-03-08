527210b759d4fbc38ad7b8e1abc22777bebcad0d
anavollu <anavollu@id.uff.br>
2023-03-08 20:58:18 -0300

    fix: contact info

--- src/routes/contato/+page.svelte
+++ src/routes/contato/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import ContactCard from '../contact-card.svelte';
 	export let data;
-	const contact = data.resume.contact;
+	const { title, ...contact } = data.resume.contact;
 </script>
 
 <div class="flex w-full flex-col gap-3">
-	{#each Object.entries(contact).slice(1) as [_, { info, link, icon, description }]}
+	{#each Object.values(contact) as { info, link, icon, description }}
 		<ContactCard {info} {link} {icon} {description} />
 	{/each}
 </div>
