3516b7e4a2d995612f225764e24d05d13e17a36c
anavollu <anavollu@id.uff.br>
2023-03-09 07:52:50 -0300

    feat: add target blank on contact links

--- src/routes/contact-card.svelte
+++ src/routes/contact-card.svelte
@@ -9,6 +9,12 @@
 	<img class="absolute" width={40} src={icon} alt="Ícone" />
 	<div class="mx-6 mt-2 flex w-full flex-col items-center gap-1 rounded-2xl bg-white py-8">
 		<p class="font-poppins text-sm tracking-widest text-darkblue">{info}</p>
-		<a id="link" class="font-poppins text-sm tracking-widest text-red" href={link}>{description}</a>
+		<a
+			id="link"
+			target="_blank"
+			rel="noreferrer"
+			class="font-poppins text-sm tracking-widest text-red"
+			href={link}>{description}</a
+		>
 	</div>
 </div>
