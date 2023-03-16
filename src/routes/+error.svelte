92f22f4de212dd3c48a11401f53d98d669b89578
anavollu <anavollu@id.uff.br>
2023-03-16 16:22:04 -0300

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import errorImage from '$lib/assets/not-found.gif';
+</script>
+
+<div class="flex h-[400px] flex-col items-center justify-center gap-6 rounded-2xl bg-white">
+	<img src={errorImage} alt="Imagem de erro" width="300" />
+	<p class="mt-6 text-center font-poppins text-lg tracking-widest text-blue">
+		Oooops! Página não encontrada!
+	</p>
+</div>
