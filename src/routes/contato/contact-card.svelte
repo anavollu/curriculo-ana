13dc89c398cd0c80ca7c3c6815f4ba4a09088dba
anavollu <anavollu@id.uff.br>
2023-04-04 17:50:37 -0300

    fix: contact card small width

--- src/routes/contato/contact-card.svelte
+++ src/routes/contato/contact-card.svelte
@@ -8,7 +8,9 @@
 
 <div class="flex w-full">
 	<img class="absolute" width={40} src={icon} alt="Ícone" />
-	<div class="mx-6 mt-2 flex w-full flex-col items-center gap-1 rounded-2xl bg-white py-8">
+	<div
+		class="mx-6 mt-2 grid w-full grid-cols-1 gap-1 break-words rounded-2xl bg-white py-8 text-center"
+	>
 		<p class="font-poppins text-sm tracking-widest text-darkblue">{info}</p>
 		{#if type === 'link'}
 			<a
