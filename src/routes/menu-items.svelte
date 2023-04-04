010eebf5f923711c70f395b769ad616f4e1e7a43
anavollu <anavollu@id.uff.br>
2023-04-04 17:17:05 -0300

    fix: remove resume button on menu

--- src/routes/menu-items.svelte
+++ src/routes/menu-items.svelte
@@ -12,14 +12,16 @@
 		class="absolute top-[75px] z-10 w-[230px] flex-col justify-center rounded-[10px] bg-candyblue"
 	>
 		{#each pages as { name, path }}
-			<button
-				on:click={() => {
-					menuOpen = !menuOpen;
-				}}
-				class="w-full rounded-[10px] py-[10.5px] px-[25px] font-poppins text-lg text-blue hover:bg-blue hover:text-candyblue"
-			>
-				<a href={path}>{name}</a>
-			</button>
+			{#if name !== 'Currículo'}
+				<button
+					on:click={() => {
+						menuOpen = !menuOpen;
+					}}
+					class="w-full rounded-[10px] py-[10.5px] px-[25px] font-poppins text-lg text-blue hover:bg-blue hover:text-candyblue"
+				>
+					<a href={path}>{name}</a>
+				</button>
+			{/if}
 		{/each}
 	</div>
 {/if}
