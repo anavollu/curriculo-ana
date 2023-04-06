b4b7057a53ef7625e1b84b3eab862e39fca97667
anavollu <anavollu@id.uff.br>
2023-04-06 20:47:45 -0300

    fix: resume desktop page

--- src/routes/curriculo/+page.svelte
+++ src/routes/curriculo/+page.svelte
@@ -11,20 +11,22 @@
 	const { title: contactTitle, ...contact } = data.resume.contact;
 </script>
 
-<div class="flex flex-col bg-offwhite px-3 pt-7 pb-[23px] font-poppins print:w-[595px] lg:px-7">
+<div
+	class="flex flex-col bg-offwhite px-3 pt-7 pb-[23px] font-poppins print:w-[595px] lg:gap-3 lg:px-7"
+>
 	<div class="mb-5 flex flex-col gap-2">
 		<p class="text-3xl text-darkblue">{home.title}</p>
 		<p class="font-inter uppercase leading-[19px] tracking-wider text-blue">{home.description}</p>
 	</div>
 	<!-- sobre mim -->
 	<div class="mb-5 flex flex-col gap-3">
-		<p class="text-xl leading-10 text-darkblue">{about.title}</p>
-		<p class="text-sm leading-5 text-darkblue">{about.description}</p>
+		<p class="text-xl leading-10 text-darkblue lg:text-2xl">{about.title}</p>
+		<p class="text-sm leading-5 text-darkblue lg:text-base">{about.description}</p>
 	</div>
 	<!-- habilidade -->
 	<div class="mb-5 flex flex-col gap-4">
-		<p class="text-xl leading-10 text-darkblue">{abilityTitle}</p>
-		<div class="grid grid-cols-1 gap-7 print:grid-cols-4">
+		<p class="text-xl leading-10 text-darkblue lg:text-2xl">{abilityTitle}</p>
+		<div class="grid grid-cols-1 gap-7">
 			{#each Object.entries(carouselItems) as [techName, techAbilities]}
 				<div class="flex flex-col gap-4">
 					<p
@@ -32,9 +34,9 @@
 					>
 						{techName.replace('_', ' ')}
 					</p>
-					<div class="flex flex-col gap-3">
+					<div class="flex flex-col gap-3 lg:grid lg:grid-cols-5 lg:gap-7">
 						{#each techAbilities as { name, icon }}
-							<div class="flex items-center gap-4">
+							<div class="flex gap-4 lg:flex lg:items-center lg:gap-2">
 								<img class="h-[24px] w-[24px]" src={icon} alt={`Ícone ${name}`} />
 								<p class="text-sm leading-[15px] tracking-widest text-blue">{name}</p>
 							</div>
@@ -46,19 +48,19 @@
 	</div>
 	<!-- projeto -->
 	<div class="mb-5 flex flex-col gap-4">
-		<p class="text-xl leading-10 text-darkblue">{projectTitle}</p>
+		<p class="text-xl leading-10 text-darkblue lg:text-2xl">{projectTitle}</p>
 		<div class="grid grid-cols-1 gap-10 lg:grid-cols-2">
 			{#each Object.entries(projects) as [_, projectItem]}
 				<div class="flex flex-col gap-3">
 					<img
-						class="h-auto max-w-[360px]"
+						class="h-auto max-w-[360px] lg:max-h-[170px] lg:min-h-[170px] lg:max-w-[fit-content]"
 						src={projectItem.image}
 						alt={`Imagem ${projectItem.name}`}
 					/>
 					<p class="text-sm font-medium uppercase leading-[15px] tracking-widest text-darkblue">
 						{projectItem.name}
 					</p>
-					<p class="text-sm leading-4 text-darkblue">
+					<p class="text-sm leading-4 text-darkblue lg:text-base">
 						{projectItem.description}
 					</p>
 					<a href={projectItem.vercelLink} class="text-xs leading-[12px] text-blue underline">
@@ -70,15 +72,17 @@
 	</div>
 	<!-- formação -->
 	<div class="mb-5 flex flex-col gap-4">
-		<p class="text-xl leading-10 text-darkblue">{educationTitle}</p>
-		<div class="grid grid-cols-1">
+		<p class="text-xl leading-10 text-darkblue lg:text-2xl">{educationTitle}</p>
+		<div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-y-4 lg:gap-x-8">
 			{#each Object.values(education) as educationItem}
-				<div class="flex items-center gap-2 border-l-[1px] border-lightblue py-1">
-					<div class="flex items-center">
+				<div
+					class="flex items-center gap-2 border-l-[1px] border-lightblue py-1 lg:items-start lg:border-none"
+				>
+					<div class="flex items-center lg:hidden">
 						<div class="h-[1px] w-[20px] bg-lightblue" />
 						<div class="h-[10px] w-[10px] rounded-full bg-lightblue" />
 					</div>
-					<div class="flex flex-col gap-2 p-2">
+					<div class="flex flex-col gap-2 p-2 lg:px-0">
 						<p class="text-sm font-medium uppercase leading-[15px] tracking-widest text-darkblue">
 							{educationItem.name}
 						</p>
@@ -94,8 +98,8 @@
 	</div>
 	<!-- contato -->
 	<div class="flex flex-col gap-4">
-		<p class="text-xl leading-10 text-darkblue">{contactTitle}</p>
-		<div class="grid grid-cols-1 gap-3 lg:grid-cols-4">
+		<p class="text-xl leading-10 text-darkblue lg:text-2xl">{contactTitle}</p>
+		<div class="grid grid-cols-1 gap-3 lg:grid-cols-2">
 			{#each Object.values(contact) as contactItem}
 				<ContactCard {...contactItem} isResumePage={true} />
 			{/each}
